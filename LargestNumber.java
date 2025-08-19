10672e4a71223d1a5ed79e16a46ec59c4cb69a45
Ashutosh Yadav <144024653+journeywithashu@users.noreply.github.com>
2025-08-19 17:49:44 +0530

    Organised array files into Array Folder

---
+++ LargestNumber.java
@@ -0,0 +1,24 @@
+public class LargestNumber {
+     public static int getLargest(int numbers[]){
+          int largest = Integer.MIN_VALUE;
+          int smallest = Integer.MAX_VALUE;
+          for(int i = 0 ; i < numbers.length ; i++){
+               if(largest<numbers[i]){
+                    largest = numbers[i];
+               }if(smallest>numbers[i]){
+                    smallest = numbers[i];
+               }
+          }
+          System.out.println(smallest);
+          return largest;
+          
+
+     }
+     public static void main(String args[]){
+          int numbers[] = {2,5,6,5,1};
+          System.out.println(getLargest(numbers));
+          
+
+     }
+     
+}
---
+++ Stocks.java
@@ -0,0 +1,21 @@
+public class Stocks {
+     public static int buyAndSellStocks(int prices[]){
+       int buyPrice = Integer.MAX_VALUE;
+       int maxProfit = 0;
+
+       for(int i = 0;i<prices.length;i++){
+          if(buyPrice<prices[i]){//profit
+               int profit = prices[i]-buyPrice;//todays profit
+               maxProfit = Math.max(maxProfit,profit);
+       }else{
+          buyPrice = prices[i];
+       }
+     }
+      return maxProfit;
+     }
+     public static void main(String[] args) {
+         int price [] = {7,1,5,3,6,4};
+         System.out.println(buyAndSellStocks(price));
+     }
+     
+}
