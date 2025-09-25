a0ec01aa28a750ecd06abf8f4176db20c5756d55
Ashutosh Yadav <144024653+journeywithashu@users.noreply.github.com>
2025-09-25 05:50:22 +0530

    recursion

---
+++ Recursion/RecursionBasics.java
@@ -0,0 +1,35 @@
+public class RecursionBasics {
+     public static void printDec(int n){
+          if(n == 1){
+               System.out.print(1);
+               return;
+          }
+          System.out.print(n+" ");
+          printDec(n-1);
+     }
+     public static void printInc(int n){
+          if(n == 1){
+               System.out.print(n+" ");
+               return;
+          }
+          printInc(n-1);
+          System.out.print(n+" ");
+
+     }
+     //PROBLEM 3
+
+     public static int fact(int n){
+          if(n == 0){
+               return 1;
+          }
+          int fnm1 = fact(n-1);
+          int fn = n*fact(n-1);
+          return fn;
+     }
+     public static void main(String[] args) {
+         int n = 5;
+        // printDec(n);
+         //printInc(n);
+         System.out.println(fact(n));
+     }
+}
