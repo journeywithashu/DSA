66951c606adf4fd262184ea9b564ebc76fe5e92d
Ashutosh Yadav <144024653+journeywithashu@users.noreply.github.com>
2025-09-25 06:51:07 +0530

    Fibbnacci series

--- Recursion/RecursionBasics.java
+++ Recursion/RecursionBasics.java
@@ -36,11 +36,20 @@ public class RecursionBasics {
           int sn = n + Snm1;
           return sn;
       }
+     
+      //Problem- Fibonacci Number
+      public static int fib(int n){
+          if(n == 0 || n == 1) return n;
+          int fnm1 = fib(n-1);
+          int fnm2 = fib(n-2);
+          int fn = fnm1 + fnm2;
+          return fn;
+      }
 
      public static void main(String[] args) {
          int n = 5;
         // printDec(n);
          //printInc(n);
-         System.out.println(calSum(n));
+         System.out.println(fib(n));
      }
 }
